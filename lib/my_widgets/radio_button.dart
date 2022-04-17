2a1898cc7677377855faff66f107dda0d716b8b0
Mona ElSaka <monaelsaka2222@gmail.com>
2022-04-17 09:19:29 +0200

    file added

--- lib/my_widgets/radio_button.dart
+++ lib/my_widgets/radio_button.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:selectable_container/selectable_container.dart';
 
 enum OS { provider, asker }
 
@@ -12,82 +12,56 @@ class RadioButton extends StatefulWidget {
 
 class _RadioButtonState extends State<RadioButton> {
   OS? _os = OS.asker;
-  // bool _ActiveColor = false;
-
+  bool _select1 = false;
+  bool _select2 = false;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        InkWell(
-          onTap: (){},
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(35),
-                color: Colors.yellow[100],
-                border: Border.all(
-                  color: Colors.cyan,
-                  width: 5,
-                )),
-            child: Column(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.only(left: 100),
-                    child: Radio(
-                        value: OS.asker,
-                        activeColor: Colors.cyan,
-                        groupValue: _os,
-                        onChanged: (OS? value) {
-                          print("T");
-                          setState(() {
-                            _os = value;
-                          });
-                        }
+        SelectableContainer(
+          selectedBorderColor: Colors.cyan.shade700,
+          unselectedBorderColor: Colors.grey.shade600,
 
-                        )
-                ),
-                Image.network(
-                  "https://photo333.com/circlecropphoto.png",
-                  width: 150,
-                  height: 150,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(13.0),
-                  child: Text("service provider"),
-                )
-              ],
-            ),
-          ),
-        ),
-        Container(
-          height: 150,
-          width: 20,
-        ),
-        Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(35),
-              color: Colors.yellow[100],
-              border: Border.all(
-                color: Colors.grey,
-                width: 5,
-              )),
+          selected: _select1,
+          onValueChanged: (newValue) {
+            setState(() {
+              _select1 = newValue;
+
+            });
+          },
           child: Column(
             children: [
-              Padding(
-                  padding: const EdgeInsets.only(left: 100),
-                  child: Radio(
-                      value: OS.provider,
-                      activeColor: Colors.cyan,
-                      groupValue: _os,
-                      onChanged: (OS? value) {
-                        print("T");
-                        setState(() {
-                          _os = value;
-                        });
-                      })),
               Image.network(
                 "https://photo333.com/circlecropphoto.png",
                 height: 150,
-                width: 150,
+                width: 130,
+
+              ),
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: Text("service provider"),
+              )
+            ],
+          ),
+        ),
+SizedBox(width: 50,),
+        SelectableContainer(
+          selectedBorderColor: Colors.cyan.shade700,
+          unselectedBorderColor: Colors.grey.shade600,
+
+          selected: _select2,
+          onValueChanged: (newValue) {
+            setState(() {
+              _select2 = newValue;
+
+            });
+          },
+          child: Column(
+            children: [
+     Image.network(
+                "https://photo333.com/circlecropphoto.png",
+                height: 150,
+                width: 130,
               ),
               Padding(
                 padding: const EdgeInsets.all(13.0),
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -15,44 +15,53 @@ class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 30.0, top: 30),
-            child: Container(
-                alignment: Alignment.topLeft,
-                child: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: Icon(Icons.arrow_back))),
+      body:
+      SingleChildScrollView(
+        child: Container(
+          // width: MediaQuery.of(context).size.width/2.5,
+          // height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30.0, top: 30),
+                child: Container(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(Icons.arrow_back))),
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    Image.network(
+                      "https://cdn130.picsart.com/273835927026211.png", width: 60,
+                      height: 60,),
+                    TitleSecondScreen(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0),
+                child: Title2ScondScreen(),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Title3ScondScreen(),
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: RadioButton(),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 140.0),
+                child: Continue(),
+              ),
+            ],
           ),
-          Center(
-           child:   Column(
-             children: [
-               Image.network("https://cdn130.picsart.com/273835927026211.png",width: 60,height: 60,),
-               TitleSecondScreen(),
-             ],
-           ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Title2ScondScreen(),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Title3ScondScreen(),
-          ),
-          SizedBox(height: 20,),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: RadioButton(),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 140.0),
-            child: Continue(),
-          ),
-        ],
+        ),
       ),
     );
   }
